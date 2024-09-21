99f176ea2a457a29642a7e217a7e4f555c2d34ad
Nguyễn Trung Hiếu <145263187+nghieee@users.noreply.github.com>
2024-09-21 08:36:04 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DoAnPM_TH_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
