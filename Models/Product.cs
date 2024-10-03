5a0f1c8d90013f116cc6c853720c6ddb7be5fba4
Nguyễn Trung Hiếu <145263187+nghieee@users.noreply.github.com>
2024-10-03 20:36:38 +0700

    update

---
+++ Models/Product.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DoAnPM_TH_.Models;
+
+public partial class Product
+{
+    public string ProId { get; set; } = null!;
+
+    public string? ProName { get; set; }
+
+    public double? OldPrice { get; set; }
+
+    public double? NewPrice { get; set; }
+
+    public int? UnitPrice { get; set; }
+
+    public string? Descript { get; set; }
+
+    public int? Quantity { get; set; }
+
+    public string? Origin { get; set; }
+
+    public string? Status { get; set; }
+
+    public string? CateId { get; set; }
+
+    public string? BrandId { get; set; }
+
+    public string? ManId { get; set; }
+
+    public double? Rating { get; set; }
+
+    public string? ProImg { get; set; }
+
+    public virtual Brand? Brand { get; set; }
+
+    public virtual Category? Cate { get; set; }
+
+    public virtual Manufacturer? Man { get; set; }
+
+    public virtual ICollection<OrderDetail> OrderDetails { get; } = new List<OrderDetail>();
+
+    public virtual ICollection<Rating> Ratings { get; } = new List<Rating>();
+
+    public virtual ICollection<Voucher> Vouchers { get; } = new List<Voucher>();
+
+    public virtual ICollection<User> Users { get; } = new List<User>();
+}
