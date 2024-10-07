41ff515d18d8ea61f96eee7ebd6929e7881cff3f
Nguyễn Trung Hiếu <145263187+nghieee@users.noreply.github.com>
2024-10-07 10:05:34 +0700

    ok

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using DoAnPM_TH_.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoAnPM_TH_.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly LongChauStoreContext _context;
+
+        public ProductController(LongChauStoreContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
---
+++ ViewModels/ProductCategoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using DoAnPM_TH_.Models;
+
+namespace DoAnPM_TH_.ViewModels
+{
+    public class ProductCategoryViewModel
+    {
+        public Category Category { get; set; } // Danh mục
+        public List<Product> Products { get; set; } // Danh sách sản phẩm thuộc danh mục
+    }
+}
