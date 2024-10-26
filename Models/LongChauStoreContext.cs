579c0c8945cc82398660c454deaa14538773eddb
C3 <thvinhhtran29@gmail.com>
2024-10-26 07:40:12 +0700

    first commit

--- Models/LongChauStoreContext.cs
+++ Models/LongChauStoreContext.cs
@@ -37,7 +37,7 @@ public partial class LongChauStoreContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-NKSMOFR\\SQLEXPRESS;Initial Catalog=LongChauStore;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-J5ACC5V\\SQLEXPRESS;Initial Catalog=LongChauStore;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
